73b95014fb1d46c51cd5ee942409cf3ad0d7cc23
Jessica Taylor <jessi.liu.taylor@gmail.com>
2023-05-10 08:57:55 -0400

    fixed choice thing

---
+++ fixed-choice/fixed-choice.py
@@ -0,0 +1,17 @@
+import openai
+
+
+def get_completion(messages, max_tokens=100):
+    return openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            max_tokens=max_tokens,
+            messages=messages)['choices'][0]['message']['content']
+
+def preference_option_score(pref, option):
+    prompt = 'The following user has these preferences: "' + pref + '".\n' + \
+            'Here is an option available to them:\n "' + option + '".\n' + \
+            'Predict how good this option is according to their preferences, from 0-1000. Just say the number, nothing else.'
+    return get_completion([{'role': 'user', 'content': prompt}])
+
+print(preference_option_score('I like to eat healthy food.', 'This restaurant sells vegetables and noodles'))
+print(preference_option_score('I like to eat healthy food.', 'This restaurant sells hamburgers and fries'))
