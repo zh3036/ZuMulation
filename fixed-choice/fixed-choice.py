057c69d1e9fde5fee29ae14e680e58859c08f15f
Jessica Taylor <jessi.liu.taylor@gmail.com>
2023-05-10 09:04:21 -0400

    int parsing

--- fixed-choice/fixed-choice.py
+++ fixed-choice/fixed-choice.py
@@ -1,4 +1,5 @@
 import openai
+import re
 
 
 def get_completion(messages, max_tokens=100):
@@ -11,7 +12,13 @@ def preference_option_score(pref, option):
     prompt = 'The following user has these preferences: "' + pref + '".\n' + \
             'Here is an option available to them:\n "' + option + '".\n' + \
             'Predict how good this option is according to their preferences, from 0-1000. Just say the number, nothing else.'
-    return get_completion([{'role': 'user', 'content': prompt}])
+    while True:
+        text = get_completion([{'role': 'user', 'content': prompt}])
+        num_text = re.search('\d+', text)
+        if num_text:
+            return int(num_text.group(0))
+        print('Could not find integer in text: ' + text)
 
 print(preference_option_score('I like to eat healthy food.', 'This restaurant sells vegetables and noodles'))
 print(preference_option_score('I like to eat healthy food.', 'This restaurant sells hamburgers and fries'))
+print(preference_option_score('I like to eat healthy food.', 'This restaurant sells fish and salad'))
