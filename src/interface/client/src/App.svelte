95f26fbfb5c5de80c09052797117412a2ecc12ce
Jessica Taylor <jessi.liu.taylor@gmail.com>
2023-05-11 12:11:23 -0400

    options

--- src/interface/client/src/App.svelte
+++ src/interface/client/src/App.svelte
@@ -1,15 +1,14 @@
 <script>
+  import Options from './Options.svelte';
 	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Options/>
 </main>
 
 <style>
 	main {
-		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
---
+++ src/interface/client/src/Options.svelte
@@ -0,0 +1,23 @@
+
+<script>
+  export let options = [];
+</script>
+
+<div>
+  {#each options as option}
+    <div>
+      <label for="name">Option name:</label>
+      <input name="name" type="text" bind:value={option.name} />
+      <br/>
+      <label for="enabled">Enable this option:</label>
+      <input name="enabled" type="checkbox" bind:checked={option.enabled} />
+      <br/>
+      <label for="description">Option description:</label>
+      <br/>
+      <textarea name="description" rows=6 cols=50 bind:value={option.description}></textarea>
+    </div>
+  {/each}
+</div>
+<button type="button" on:click={() => options = [...options, { name: '', enabled: true, description: '' }]}>
+  Add option
+</button>
