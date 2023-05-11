1a88f95aacea2ae6c4386ccb047e4bc73609b410
Jessica Taylor <jessi.liu.taylor@gmail.com>
2023-05-11 10:05:10 -0400

    web server template

---
+++ src/interface/webserver.py
@@ -0,0 +1,14 @@
+
+import sqlite3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/')
+def index():
+    return render_template('index.html')
