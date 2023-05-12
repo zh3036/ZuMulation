91cacc1babb7d92ce11f914ea794a78e6ee3ffc8
Lev Stambler <levstamb@gmail.com>
2023-05-12 07:35:11 +0100

    baseline objections

--- src/llm/iteration.py
+++ src/llm/iteration.py
@@ -3,13 +3,16 @@ from langchain.llms import OpenAI
 from langchain.output_parsers import CommaSeparatedListOutputParser
 from langchain.memory import ConversationBufferMemory
 
-class LoopBack:
+class Iteration:
     def __init__(self, natural_problem_statement: str, decision: str, option: str, llm=None):
-        self.llm = llm if llm is not None else OpenAI(temperature=0, model_name="gpt4")
+        self.llm = llm if llm is not None else OpenAI(temperature=0, model_name="GPT-4")
         self.prob_statement = natural_problem_statement
         self.decision = decision
 
     def create_natural_objection(self, user_input) -> str:
+        """
+        Create a natural language objection to the decision
+        """
         conversation = ConversationChain(
             llm=self.llm,
             memory=ConversationBufferMemory(),
@@ -21,9 +24,19 @@ A decision has been made regarding {self.prob_statement}. The decision is to {se
 What is your biggest objection to this decision? Please write a sentence or two.""")
         return ret
 
-    def update_user(self, user, objections):
+    def update_user(self, user_input, objections):
+        """
+        Update the user's beliefs and values based on the objections 
+        """
         conversation = ConversationChain(
             llm=self.llm,
             memory=ConversationBufferMemory(),
             verbose=False
         )
+        ret = conversation.predict(input=f"""You are acting as a person. Here is the person:
+{user_input}
+A decision has been made regarding {self.prob_statement}. The decision is to {self.decision}.
+Different people have different objections to this decision. Here are some objections:
+{objections}
+Consider these objections and update your set of beliefs and values. What is your new set of beliefs and values? Please write a sentence or two.""")
+        return ret
