ec0bce5350cde7e7bdeec8b5ec916cd56f1d62a5
FIKS <zh3036@gmail.com>
2023-05-13 21:53:49 +0200

    some indentation

--- src/natural_formal_translator/iteration.py
+++ src/natural_formal_translator/iteration.py
@@ -28,11 +28,11 @@ class Iteration:
             verbose=False
         )
         ret = conversation.predict(input=f"""You are acting as a voter. Here is the person's description: 
-{user_input}
-An outcome has been created regarding {self.prob_statement}, after collecting every citizen's vote. 
-The result of quadratic funding allocation is:\n {self.finalDecisions}.
-According to your persona,
-What is your biggest objection to this funding allocation results? Please write two objections.""")
+            {user_input}
+            An outcome has been created regarding {self.prob_statement}, after collecting every citizen's vote. 
+            The result of quadratic funding allocation is:\n {self.finalDecisions}.
+            According to your persona,
+            What is your biggest objection to this funding allocation results? Please write two objections.""")
         return ret
 
     def summarize_objections(self, objections: list[str]) -> str:
@@ -61,22 +61,23 @@ What is your biggest objection to this funding allocation results? Please write 
             verbose=False
         )
         ret = conversation.predict(input=f"""You are acting as a voter. Here is the person's description: 
-{citizen_persona}
-An outcome has been created regarding {self.prob_statement}, after collecting every citizen's vote. 
-The result of quadratic funding allocation is:\n {self.finalDecisions}.
-According to your persona,
-What is your biggest objection to this funding allocation results? Please write two objections.
-Different people have different objections to this decision. Here are some objections summary:
-{objections_summary}
-you original vote for {options} are {original_vote} respectively which contributes the final allocation through quadratic funding.
-we want you to respond to the objection summary, 
-give your new vote if you would like change, and give your reasons for each change/or no change
-If you want to change your vote, please write down your new vote for each option. 
-if you donot want to change your vote, please output your original vote for each option.
-notice there are {str(len(options))} options
-your output should be in format of 
-$&$ [option1 vote, option2 vote, option3 vote, ...] $&$ reasons for each change/or no change
-""")
+            {citizen_persona}
+            An outcome has been created regarding {self.prob_statement}, after collecting every citizen's vote. 
+            The result of quadratic funding allocation is:\n {self.finalDecisions}.
+            According to your persona,
+            What is your biggest objection to this funding allocation results? Please write two objections.
+            Different people have different objections to this decision. Here are some objections summary:
+            {objections_summary}
+            you original vote for {options} are {original_vote} respectively which contributes the final allocation through quadratic funding.
+            we want you to respond to the objection summary, 
+            give your new vote if you would like change, and give your reasons for each change/or no change
+            If you want to change your vote, please write down your new vote for each option. 
+            if you donot want to change your vote, please output your original vote for each option.
+            notice there are {str(len(options))} options
+            your output should be in format of 
+            $&$ [option1 vote, option2 vote, option3 vote, ...] $&$ reasons for each change/or no change
+            """)
+        print(ret)
         return ret
     
     def get_formatted_votes(self,update_msg:str, num_options:int)->list[float]:
@@ -114,30 +115,31 @@ if __name__ == "__main__":
     output_file.write("\nxxxxxxx\nxxxxxxxxx\nxxxxxxxx\n\n")
     sys.stdout = output_file
     llm = Anthropic(model="claude-instant-v1-100k")
+    # llm = OpenAI(temperature=0, model_name="gpt-4")
     citizen_descs=["""Citizen 1: Mary Green
-Persona: Environmental enthusiast, age 32, single, yoga instructor
-General Opinion on Zuzalu: Excited about the idea of living in an ecologically focused environment promoting a strong, healthy sense of community.
-Attitudes towards projects: Highly interested in health and wellness, renewable energy, and ecosystem conservations. Appreciates access to shared resources, creative culture, and sustainable transportation methods.""",
-                   """Citizen 2: Tom Sanders
-Persona: Entrepreneur, age 40, married, two kids, involved in the tech industry
-General Opinion on Zuzalu: Sees it as a smart way of living, intrigued by innovation and creative opportunities offered in such a space.
-Attitudes towards projects: Looks forward to collaborative workspaces, skill-sharing opportunities, and sustainable solutions in energy and food production. Appreciates health and wellness initiatives to achieve a better work-life balance.""",
-                   """Citizen 3: Rachel Thompson
-Persona: Artist, age 29, in a relationship, mural artist
-General Opinion on Zuzalu: Loves the idea of a creative and culturally thriving space with exciting opportunities for artists and creators.
-Attitudes towards projects: Enthusiastic about art and culture projects, community garden programs, and anything related to enhancing creativity. Appreciates the community-based actions leading to the environmental stewardship of multiple projects."""]
+        Persona: Environmental enthusiast, age 32, single, yoga instructor
+        General Opinion on Zuzalu: Excited about the idea of living in an ecologically focused environment promoting a strong, healthy sense of community.
+        Attitudes towards projects: Highly interested in health and wellness, renewable energy, and ecosystem conservations. Appreciates access to shared resources, creative culture, and sustainable transportation methods.""",
+                        """Citizen 2: Tom Sanders
+        Persona: Entrepreneur, age 40, married, two kids, involved in the tech industry
+        General Opinion on Zuzalu: Sees it as a smart way of living, intrigued by innovation and creative opportunities offered in such a space.
+        Attitudes towards projects: Looks forward to collaborative workspaces, skill-sharing opportunities, and sustainable solutions in energy and food production. Appreciates health and wellness initiatives to achieve a better work-life balance.""",
+                        """Citizen 3: Rachel Thompson
+        Persona: Artist, age 29, in a relationship, mural artist
+        General Opinion on Zuzalu: Loves the idea of a creative and culturally thriving space with exciting opportunities for artists and creators.
+        Attitudes towards projects: Enthusiastic about art and culture projects, community garden programs, and anything related to enhancing creativity. Appreciates the community-based actions leading to the environmental stewardship of multiple projects."""]
 
     # voteRes = [4, 20, 3.5, 10, 5]
     options=["""Project 1 - Sustainable Food Production System 
-Explanation: As an aspiring chef with a culinary education, I am passionate about turning locally-grown and sustainably sourced ingredients into delicious culinary creations. I believe this project could provide valuable resources and inspire community residents, including myself, in innovative and sustainable cooking practices while also reducing our carbon footprint.""",
-               """Project 2 - Skill-Sharing and Educational Initiatives 
-Explanation: We can never stop learning, improving and sharing our knowledge with others. Combining culinary education and experience with others' skill sets will only lead to a more vibrant and multifaceted community. I look forward to expanding my own horizons as well as having the opportunity to teach cooking classes and share my passion for food with my fellow community members.""",
-               """Project 3 - Transportation and Mobility Promoting 
-Explanation: Being an advocate for healthy living and sustainability, ensuring efficient and environment-friendly mobility options is important. Commuting to local farmers' markets and other food sourcing without negatively impacting the environment is both practical and appealing for me. Investing in bike-shares and community-friendly pathways will also encourage a more active lifestyle and foster community-wide connections.""",
-               """Project 4 - Health and Wellness Program 
-Explanation: Total well-being comprises more than just eating right; a healthy mind and body are of vital importance too. Yoga, meditation, and holistic therapies resonate deeply with my desire to live a well-rounded life. This project could attract health professionals from whom I can learn and truly cultivate a wholesome atmosphere in our community.""",
-               """Project 5 - Art and Culture Enrichment 
-Explanation: A healthy dose of art and culture can breathe life into any community. As an appreciator of art, I believe dedicated gallery spaces, workshops, and other creative projects will not only foster our home-grown artistic expressions but also incorporate creativity into our everyday lives, potentially blending with food production and healthy living."""]
+        Explanation: As an aspiring chef with a culinary education, I am passionate about turning locally-grown and sustainably sourced ingredients into delicious culinary creations. I believe this project could provide valuable resources and inspire community residents, including myself, in innovative and sustainable cooking practices while also reducing our carbon footprint.""",
+                    """Project 2 - Skill-Sharing and Educational Initiatives 
+        Explanation: We can never stop learning, improving and sharing our knowledge with others. Combining culinary education and experience with others' skill sets will only lead to a more vibrant and multifaceted community. I look forward to expanding my own horizons as well as having the opportunity to teach cooking classes and share my passion for food with my fellow community members.""",
+                    """Project 3 - Transportation and Mobility Promoting 
+        Explanation: Being an advocate for healthy living and sustainability, ensuring efficient and environment-friendly mobility options is important. Commuting to local farmers' markets and other food sourcing without negatively impacting the environment is both practical and appealing for me. Investing in bike-shares and community-friendly pathways will also encourage a more active lifestyle and foster community-wide connections.""",
+                    """Project 4 - Health and Wellness Program 
+        Explanation: Total well-being comprises more than just eating right; a healthy mind and body are of vital importance too. Yoga, meditation, and holistic therapies resonate deeply with my desire to live a well-rounded life. This project could attract health professionals from whom I can learn and truly cultivate a wholesome atmosphere in our community.""",
+                    """Project 5 - Art and Culture Enrichment 
+        Explanation: A healthy dose of art and culture can breathe life into any community. As an appreciator of art, I believe dedicated gallery spaces, workshops, and other creative projects will not only foster our home-grown artistic expressions but also incorporate creativity into our everyday lives, potentially blending with food production and healthy living."""]
 
 # simulate some vote
 # vote_by_citiziens is a matrix such that every entry is one citizen's vote for each options, initialzed with random values between 0 and 10
