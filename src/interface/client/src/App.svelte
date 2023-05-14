4a40bf29218bf6d918ec6e6d9c06d9c1bbf69851
Jessica Taylor <jessi.liu.taylor@gmail.com>
2023-05-14 08:23:05 -0400

    matrix.svelte

--- src/interface/client/src/App.svelte
+++ src/interface/client/src/App.svelte
@@ -1,17 +1,23 @@
 <script>
   import Proposals from './Proposals.svelte';
   import Voters from './Voters.svelte';
+  import Matrix from './Matrix.svelte';
+
+  let proposals = [];
+  let voters = [];
+
 </script>
 
 <main>
   <div class="row">
     <div class="column">
-      <Proposals/>
+      <Proposals bind:proposals />
     </div>
     <div class="column">
-      <Voters/>
+      <Voters bind:voters />
     </div>
   </div>
+  <Matrix bind:proposals bind:voters/>
 </main>
 
 <style>
---
+++ src/interface/client/src/Matrix.svelte
@@ -0,0 +1,36 @@
+
+<script>
+  export let proposals = [];
+  export let voters = [];
+  export let matrix = [];
+
+  function getMatrix(v, p) {
+    if (v >= matrix.length || p >= matrix[0].length) {
+      return NaN;
+    }
+    return matrix[v][p];
+  }
+</script>
+
+<div>
+  <table>
+    <thead>
+      <tr>
+        <th></th>
+        {#each proposals as proposal}
+          <th>{proposal.name}</th>
+        {/each}
+      </tr>
+    </thead>
+    <tbody>
+      {#each voters as voter, i}
+        <tr>
+          <th>{voter.name}</th>
+          {#each proposals as proposal, j}
+            <td>{getMatrix(i, j)}</td>
+          {/each}
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
